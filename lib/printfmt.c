68f906ce9bd2c3e595a5a4ebeceef7712aaa9c90
LimitW <hbdgwb13@gmail.com>
2015-12-02 23:46:15 +0800

    lab1

--- lib/printfmt.c
+++ lib/printfmt.c
@@ -200,16 +200,16 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 		// unsigned decimal
 		case 'u':
 			num = getuint(&ap, lflag);
-			base = 10;
+			base = 10; 
 			goto number;
 
 		// (unsigned) octal
 		case 'o':
 			// Replace this with your code.
-			putch('X', putdat);
-			putch('X', putdat);
-			putch('X', putdat);
-			break;
+			num = getuint(&ap, lflag);
+			base = 8;
+			goto number;
+
 
 		// pointer
 		case 'p':
